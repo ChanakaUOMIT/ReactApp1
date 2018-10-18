a3e1a06c09903aaef9cc0668e53e0351b1ed1eb1
Chanaka Sampath Wickramanayaka <chanakauomfit@gmail.com>
2018-10-18 17:37:39 +0530

    upto semantic ui setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import HomePage from './components/pages/HomePage';
+import LoginPage from './components/pages/LoginPage';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello React</h1>
-      </div>
-    );
+      <div className="ui container">
+        < Route path="/" exact component={HomePage} />
+        < Route path="/login" exact component={LoginPage} />
+       </div>
+    ); 
   }
 }
 
